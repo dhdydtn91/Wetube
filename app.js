02f3564dec3ab84f1e36000b86d230bfb71a4412
dhdydtn2019 <dhdydtn2019@gmail.com>
2019-08-28 12:50:56 +0900

    commit middleware

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { userRouter } from "./router";
+
+const app = express();
+
+const handleHome = (req, res) => res.send("Hello from ass");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+
+app.get("/user", userRouter);
+
+export default app;
