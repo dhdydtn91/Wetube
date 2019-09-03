f8f1213ae21412cec0c080ec26ae0091858ee4cf
dhdydtn2019 <dhdydtn2019@gmail.com>
2019-09-03 22:56:54 +0900

    social login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,107 @@
+import passport from "passport";
 import routes from "../routes";
-
+import User from "../models/User";
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
   if (password !== password2) {
     res.status(400);
-    res.render("join", { pageTitle: "Join" });
+    res.render("join", { pageTitle: "Join" }); //비밀번호와 비밀번호 확인이 같이 않으면 다시 회원가입
   } else {
-    //할일 : 사용자 등록
-    //할일 : 사용자 로그인
-    res.redirect(routes.home);
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password); //회원의 아이디랑 비번 등록
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home); //홈화면으로
+    }
   }
-  res.render("join", { pageTitle: "Join" });
 };
 
 export const getlogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
-export const postLogin = (req, res) => {
+
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login, //실패했을때 로그인화면으로
+  successRedirect: routes.home //성공했을때 홈화면으로
+});
+
+export const githubLogin = passport.authenticate("github"); //user의 정보를 github에 보냄
+
+export const githubLoginCallback = async (_, __, profile, cb) => {
+  const {
+    _json: { id, avatar_url: avatarUrl, name, email }
+  } = profile; //github에서 user 정보를 받아옴
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      user.githubId = id;
+      user.save();
+      return cb(null, user);
+    }
+    const newUser = await User.create({
+      email,
+      name,
+      githubId: id,
+      avatarUrl
+    });
+    return cb(null, newUser);
+  } catch (error) {
+    return cb(error);
+  }
+};
+
+export const postGithubLogIn = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const facebookLogin = passport.authenticate("facebook");
+
+export const facebookLoginCallback = (
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
+export const postFacebookLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
 export const logout = (req, res) => {
-  //할일: 로그아웃절차
+  req.logout();
   res.redirect(routes.home);
 };
 
-export const userDetail = (req, res) =>
-  res.render("userDetail", { pageTitle: "UserDetail" });
+export const getMe = (req, res) => {
+  res.render("userDetail", { pageTitle: "UserDetail", user: req.user });
+}; //req,user 현재사용자
+
+export const userDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const user = await User.findById(id);
+    res.render("userDetail", { pageTitle: "User Detail", user });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "EditProfile" });
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "ChangePassword" });
--- init.js
+++ init.js
@@ -1,10 +1,12 @@
 import "./db"; // mongo db 연결
 import dotenv from "dotenv";
 import app from "./app"; // app.js를 옴
+dotenv.config();
 import "./models/Video";
 import "./models/Comment";
+import "./models/User";
+
 // file url을 숨겨 놓을때 사용
-dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
